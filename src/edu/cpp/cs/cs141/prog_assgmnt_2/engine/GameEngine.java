3a2781b6525cb773a8271ab411f31b83796c5d99
z-ach <zachkaufman97@gmail.com>
2017-01-22 14:20:39 -0800

    added all necessary classes, and put constructors in them

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_2/engine/GameEngine.java
@@ -0,0 +1,26 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #2
+ *
+ * Mini game of escape the dungeon. Take steps forward and risk random encounters, get to the exit to win.
+ *
+ * Zach Kaufman
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_2.engine;
+
+/**
+ *
+ *
+ * @author Zach
+ */
+public class GameEngine {
+	
+	public GameEngine(){
+		
+	}
+	
+	
+	
+}
---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_2/ui/UserInterface.java
@@ -0,0 +1,24 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #2
+ *
+ * Mini game of escape the dungeon. Take steps forward and risk random encounters, get to the exit to win.
+ *
+ * Zach Kaufman
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_2.ui;
+
+/**
+ *
+ *
+ * @author Zach
+ */
+public class UserInterface {
+	
+	public UserInterface(){
+		
+	}
+	
+}
