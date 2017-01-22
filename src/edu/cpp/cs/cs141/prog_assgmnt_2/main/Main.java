d0cde05bea9086454252e7a1693cd202fcdb20c5
z-ach <zachkaufman97@gmail.com>
2017-01-22 14:00:43 -0800

    initial commit, just main class and gitignore

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_2/main/Main.java
@@ -0,0 +1,27 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #2
+ *
+ * Mini game of escape the dungeon. Take steps forward and risk random encounters, get to the exit to win.
+ *
+ * Zach Kaufman
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_2.main;
+
+/**
+ * 
+ * 
+ * @author Zach Kaufman
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
